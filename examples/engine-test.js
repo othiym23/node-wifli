e7ed5a898b879cf1b13779e23e34a1338b9fdfee
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-12-09 14:05:16 -0800

    Updated example programs based on real testing.

--- examples/engine-test.js
+++ examples/engine-test.js
@@ -4,23 +4,27 @@ var WiFli          = require('../index')
   , MockConnection = require('../lib/mock-connection')
   ;
 
-
 var kopter = new WiFli();
 // replace this with a real connect() call to send to real copter
 kopter.connection = new MockConnection();
+
 kopter.runQueue(function (tq) {
   tq.on('end', function () {
     console.log('all done!');
     kopter.end();
   });
 
-  for (var i = 1; i <= 30; i += 1) {
-    tq.enqueue({rotorSpeed : i}, 1000);
+  for (var i = 8; i <= 40; i += 1) {
+    tq.enqueue({rotorSpeed : i}, 2000);
   }
 
-  for (; i > 0; i -= 1) {
-    tq.enqueue({rotorSpeed : i}, 1000);
+  for (; i > 8; i -= 1) {
+    tq.enqueue({rotorSpeed : i}, 2000);
   }
 
   tq.enqueue({reset : true}, 0);
 });
+
+// uncomment and put the above in here for a real run
+// kopter.connect(function () {
+// });
--- examples/lifecycle.js
+++ examples/lifecycle.js
@@ -4,21 +4,26 @@ var WiFli          = require('../index')
   , MockConnection = require('../lib/mock-connection')
   ;
 
-var kopter = new WiFli();
+var kopter = new WiFli({hoverSpeed : 65, trim : -5});
 // replace this with a real connect() call to send to real copter
 kopter.connection = new MockConnection();
 
+kopter.on('launch', function () { console.log('WiFli launched'); });
+kopter.on('land', function () { console.log('WiFli landed'); });
+
 kopter.once('launch', function () {
   kopter.runQueue(function (q) {
-    q.on('end', function () { kopter.land(); });
+    q.on('end', function () { kopter.land(2500); });
 
-    q.enqueue({hover : true}, 1000);
-    q.enqueue({hover : true, trim : -8}, 500);
-    q.enqueue({hover : true, trim :  8}, 1000);
+    q.enqueue({hover : true, pitch : 30, yaw : 31}, 2000);
   });
 });
 
 kopter.once('land', function () { kopter.end(); });
 kopter.once('end', function () { console.log("all done!"); });
 
-kopter.launch();
+kopter.launch(2000);
+
+// uncomment and put the above in here for a real run
+// kopter.connect(function () {
+// });
